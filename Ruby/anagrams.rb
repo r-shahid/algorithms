d527aa72a4a5bb9d1a82a62b16280c802e9d91e1
Riana <rianashahid@gmail.com>
2020-11-15 01:27:35 -0500

    anagrams done

--- Ruby/anagrams.rb
+++ Ruby/anagrams.rb
@@ -1,20 +1,30 @@
 def anagrams?(word1, word2)
-  #take each word, separate by letter --> an array of letters
-  #alphabetize each array
-  #if two arrays are equal --> true
 
   arr1 = word1.split('')
   arr2 = word2.split('')
+    
+  counts1 = Hash.new 0
+  arr1.each do |i|
+    counts1[i] += 1
+  end
+  # p counts1
 
-  if arr1.length == arr2.length
-    p arr1.map { |i| arr2.include?(i)}.include?(true)
+  counts2 = Hash.new 0
+  arr2.each do |i|
+    counts2[i] += 1
+  end
+  # p counts2
+
+  if counts1 == counts2
+    p true
   else 
     p false
   end
 
 end
 
-anagrams?("cat", "act")    
-anagrams?("restful", "fluster")
-anagrams?("cat", "dog")      
-anagrams?("boot", "bootcamp")
+# anagrams?("cat", "act")    
+# anagrams?("restful", "fluster")
+# anagrams?("cat", "dog")      
+# anagrams?("boot", "bootcamp")
+# anagrams?("aeeaae", "eaeea")
