4d5ec50106b0fe1564d9c389194079f15dcb0ca3
Riana <rianashahid@gmail.com>
2020-11-17 22:15:23 -0500

    remove dupes done

---
+++ to_do/reverse_array.rb
@@ -0,0 +1,23 @@
+Reverse an Array in Ruby
+Given an array of numbers, write a method that returns an array with the the items in reversed order.
+
+Example:
+
+# Input:
+  reverse_arr([1, 2, 3, 4])
+    
+# Output:
+  [4, 3, 2, 1]
+Use the follow pseudocode to write this solution.
+
+ # Create function that takes an array
+ # Set variable to define a new array
+ # Create a for loop to iterate through each item in the array
+ # for each iteration, push to the new array
+ # return the new array 
+  # @param [number]
+  # @return [number]
+  
+  def reverse_arr(array)
+    # happy hacking!
+  end 
